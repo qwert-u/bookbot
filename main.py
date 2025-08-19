6d25b978947a1c7c3833fc29a1f6ed3e5a04a38d
maya <maya@Ralf.localdomain>
2025-08-19 17:37:46 +0200

    finished

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
+import sys
 from stats import *
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    chars = count_chars(text)
+    char_dict = count_chars(text)
     print(f"{num_words} words found in the document")
-    print(chars)
+    sorted_list = get_sorted_list(char_dict)
+    report(book_path, num_words, sorted_list)
+
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+
+def report(book_path, num_words, sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for c in sorted_list:
+        if c["char"].isalpha():
+            print(f"{c["char"]}: {c["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -12,3 +12,14 @@ def count_chars(text):
         else:
             chars[lowered] = 1
     return chars
+
+def sort_on(chars):
+    return chars["num"]
+
+def get_sorted_list(char_dict):
+    list_of_dicts = []
+    for c in char_dict:
+        list_of_dicts.append({"char": c, "num": char_dict[c]})
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    return list_of_dicts
+
